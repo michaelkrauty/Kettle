4b8d127d416f24556a54089ea68ca4bb9bfd2a11
michaelkrauty <michaelkrauty@gmail.com>
2014-07-25 22:39:05 -0700

    update

--- src/main/java/me/michaelkrauty/Kettle/command/backpack/BackpackCommand.java
+++ src/main/java/me/michaelkrauty/Kettle/command/backpack/BackpackCommand.java
@@ -30,70 +30,66 @@ public class BackpackCommand extends AbstractCommand {
 
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
 		final Player player = (Player) sender;
-		if (sender.hasPermission("backpack.use")) {
-			if (args.length == 0) {
-				player.sendMessage(ChatColor.GREEN + "--[Backpacks]--");
-				player.sendMessage(ChatColor.RED + "/backpack get [name]" + ChatColor.GRAY + ": Get a backpack ([name] is optional)");
-				player.sendMessage(ChatColor.RED + "/backpack name <name>" + ChatColor.GRAY + ": Name the backpack you are holding.");
-				player.sendMessage(ChatColor.DARK_GREEN + "Backpacks are physical items. You can carry as many as you want. If you drop a backpack, anyone can pick it up & get the items inside.");
-				player.sendMessage(ChatColor.DARK_RED + "WARNING: " + ChatColor.RED + "Do NOT rename a backpack with an anvil! You will never be able to access the items inside of it again!");
+		if (args.length == 0) {
+			player.sendMessage(ChatColor.GREEN + "--[Backpacks]--");
+			player.sendMessage(ChatColor.RED + "/backpack get [name]" + ChatColor.GRAY + ": Get a backpack ([name] is optional)");
+			player.sendMessage(ChatColor.RED + "/backpack name <name>" + ChatColor.GRAY + ": Name the backpack you are holding.");
+			player.sendMessage(ChatColor.DARK_GREEN + "Backpacks are physical items. You can carry as many as you want. If you drop a backpack, anyone can pick it up & get the items inside.");
+			player.sendMessage(ChatColor.DARK_RED + "WARNING: " + ChatColor.RED + "Do NOT rename a backpack with an anvil! You will never be able to access the items inside of it again!");
+			return true;
+		}
+		if (args[0].equalsIgnoreCase("get") || args[0].equalsIgnoreCase("give")) {
+			String name = "Backpack";
+			if (args.length != 1) {
+				name = "";
+				for (int i = 1; i < args.length; i++) {
+					if (i == args.length - 1)
+						name = name + args[i];
+					else
+						name = name + args[i] + " ";
+				}
+			}
+			if (player.getInventory().firstEmpty() != -1) {
+				ItemStack backpack = new ItemStack(Material.CHEST, 1);
+				ItemMeta meta = backpack.getItemMeta();
+				meta.setDisplayName(name);
+				UUID uuid = UUID.randomUUID();
+				meta.setLore(Arrays.asList(uuid.toString()));
+				backpack.setItemMeta(meta);
+				player.getInventory().addItem(backpack);
+				kettle.objects.backpacks.add(new Backpack(kettle, uuid.toString(), name));
 				return true;
 			}
-			if (args[0].equalsIgnoreCase("get") || args[0].equalsIgnoreCase("give")) {
-				String name = "Backpack";
-				if (args.length != 1) {
-					name = "";
-					for (int i = 1; i < args.length; i++) {
-						if (i == args.length - 1)
-							name = name + args[i];
-						else
-							name = name + args[i] + " ";
-					}
-				}
-				if (player.getInventory().firstEmpty() != -1) {
-					ItemStack backpack = new ItemStack(Material.CHEST, 1);
-					ItemMeta meta = backpack.getItemMeta();
-					meta.setDisplayName(name);
-					UUID uuid = UUID.randomUUID();
-					meta.setLore(Arrays.asList(uuid.toString()));
-					backpack.setItemMeta(meta);
-					player.getInventory().addItem(backpack);
-					kettle.objects.backpacks.add(new Backpack(kettle, uuid.toString(), name));
-					return true;
-				}
-				player.sendMessage(ChatColor.RED + "Your inventory is too full to give you a backpack!");
-				return true;
-			}
-			if (args[0].equalsIgnoreCase("name") || args[0].equalsIgnoreCase("rename")) {
-				if (player.getItemInHand() != null) {
-					if (player.getItemInHand().getType() == Material.CHEST) {
-						String lore;
-						if ((lore = player.getItemInHand().getItemMeta().getLore().get(0)) != null) {
-							Backpack backpack;
-							if ((backpack = kettle.objects.getBackpack(lore)) != null) {
-								String name = "";
-								for (int i = 1; i < args.length; i++) {
-									if (i == args.length - 1)
-										name = name + args[i];
-									else
-										name = name + args[i] + " ";
-								}
-								ItemMeta meta = player.getItemInHand().getItemMeta();
-								meta.setDisplayName(name);
-								player.getItemInHand().setItemMeta(meta);
-								player.sendMessage(ChatColor.GRAY + "Renamed this backpack to " + name);
-								return true;
+			player.sendMessage(ChatColor.RED + "Your inventory is too full to give you a backpack!");
+			return true;
+		}
+		if (args[0].equalsIgnoreCase("name") || args[0].equalsIgnoreCase("rename")) {
+			if (player.getItemInHand() != null) {
+				if (player.getItemInHand().getType() == Material.CHEST) {
+					String lore;
+					if ((lore = player.getItemInHand().getItemMeta().getLore().get(0)) != null) {
+						Backpack backpack;
+						if ((backpack = kettle.objects.getBackpack(lore)) != null) {
+							String name = "";
+							for (int i = 1; i < args.length; i++) {
+								if (i == args.length - 1)
+									name = name + args[i];
+								else
+									name = name + args[i] + " ";
 							}
+							ItemMeta meta = player.getItemInHand().getItemMeta();
+							meta.setDisplayName(name);
+							player.getItemInHand().setItemMeta(meta);
+							player.sendMessage(ChatColor.GRAY + "Renamed this backpack to " + name);
+							return true;
 						}
 					}
 				}
-				player.sendMessage(ChatColor.RED + "Make sure you're holding a backpack in your hand.");
-				return true;
 			}
-			player.sendMessage(ChatColor.RED + cmd.getUsage());
+			player.sendMessage(ChatColor.RED + "Make sure you're holding a backpack in your hand.");
 			return true;
 		}
-		player.sendMessage(ChatColor.RED + "You don't have permission to do that.");
+		player.sendMessage(ChatColor.RED + cmd.getUsage());
 		return true;
 	}
 }
